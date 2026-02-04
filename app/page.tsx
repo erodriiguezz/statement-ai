5eb6ca4b55b7d7694d35958b8f6b2042b6e7a39f
Ernesto Rodriguez <erodriiguezz@icloud.com>
2026-02-03 21:25:30 -0500

    added form onsubmit and file field onchange

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,30 @@ import { useState } from "react";
 export default function Home() {
   const [file, setFile] = useState(null);
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(file);
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <form action="" className="space-y-8">
+        <form className="space-y-8" onSubmit={onSubmit}>
           <div>
             <label className="block text-sm font-medium mb-2">
               Upload PDF Statements
             </label>
+
             <input
               type="file"
               name="file"
               accept="application/pdf"
               multiple
               className="block w-full text-sm text-gray-700 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:bg-gray-900 file:text-white hover:file:bg-gray-800 file:cursor-pointer"
+              onChange={(e) => setFile(e.target.files?.[0])}
             />
+
             <p className="text-xs text-gray-500 mt-2">
               Multiple PDFs supported. Files are processed temporarily and
               deleted after summary generation.
