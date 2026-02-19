f27e44ab96ded64f10c49a057e1f4fb124a17587
Ernesto Rodriguez <erodriiguezz@icloud.com>
2026-02-18 21:53:19 -0500

    reinstalled dependencies and added external package config

--- lib/pdfReader.ts
+++ lib/pdfReader.ts
@@ -1,3 +1,30 @@
+// import * as pdfjsLib from "pdfjs-dist";
+
+// export async function extractTextFromPDF(buffer: Buffer) {
+//   const uint8Array = new Uint8Array(buffer);
+
+//   // Disable worker for serverless
+//   (pdfjsLib as any).GlobalWorkerOptions.workerSrc = undefined;
+
+//   const pdf = await pdfjsLib.getDocument({
+//     data: uint8Array,
+//     useSystemFonts: true,
+//     isEvalSupported: false,
+//   }).promise;
+
+//   let text = "";
+
+//   for (let i = 1; i <= pdf.numPages; i++) {
+//     const page = await pdf.getPage(i);
+//     const content = await page.getTextContent();
+
+//     const strings = content.items.map((item: any) => item.str);
+//     text += strings.join(" ") + "\n";
+//   }
+
+//   return text;
+// }
+
 export async function extractTextFromPDF(buffer: Buffer) {
   // ⭐ prevents Next from bundling pdfjs
   const pdfjsLib = await (eval(
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
+  serverExternalPackages: ["pdfjs-dist"],
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
