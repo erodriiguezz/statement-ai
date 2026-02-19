b37ef2c44666b340223c3b7c7003f339aff01132
Ernesto Rodriguez <erodriiguezz@icloud.com>
2026-02-18 21:59:18 -0500

    updated pdfjs setup for vercel

--- lib/pdfReader.ts
+++ lib/pdfReader.ts
@@ -25,21 +25,50 @@
 //   return text;
 // }
 
+// export async function extractTextFromPDF(buffer: Buffer) {
+//   // ⭐ prevents Next from bundling pdfjs
+//   const pdfjsLib = await (eval(
+//     'import("pdfjs-dist/legacy/build/pdf.js")',
+//   ) as Promise<any>);
+
+//   const data = new Uint8Array(buffer);
+
+//   const loadingTask = pdfjsLib.getDocument({
+//     data,
+//     disableWorker: true, // critical
+//     useSystemFonts: true,
+//   });
+
+//   const pdf = await loadingTask.promise;
+
+//   let rows: string[] = [];
+
+//   for (let i = 1; i <= pdf.numPages; i++) {
+//     const page = await pdf.getPage(i);
+//     const content = await page.getTextContent();
+
+//     rows.push(...content.items.map((item: any) => item.str));
+//   }
+
+//   return rows;
+// }
+
+let pdfjsLib: any;
+
+if (typeof window === "undefined") {
+  pdfjsLib = require("pdfjs-dist/legacy/build/pdf.js");
+}
+
 export async function extractTextFromPDF(buffer: Buffer) {
-  // ⭐ prevents Next from bundling pdfjs
-  const pdfjsLib = await (eval(
-    'import("pdfjs-dist/legacy/build/pdf.js")',
-  ) as Promise<any>);
+  const uint8Array = new Uint8Array(buffer);
 
-  const data = new Uint8Array(buffer);
+  pdfjsLib.GlobalWorkerOptions.workerSrc = undefined;
 
-  const loadingTask = pdfjsLib.getDocument({
-    data,
-    disableWorker: true, // critical
+  const pdf = await pdfjsLib.getDocument({
+    data: uint8Array,
     useSystemFonts: true,
-  });
-
-  const pdf = await loadingTask.promise;
+    isEvalSupported: false,
+  }).promise;
 
   let rows: string[] = [];
 
